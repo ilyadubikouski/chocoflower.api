da8817d5dfc8cce23795a7c3a5e1edb1aa9b6bc4
ilya <dubikowsky@gmail.com>
2019-09-22 20:49:08 -0400

    add prod verision

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express v 0.01' });
+  res.render('index', { title: 'Express v 0.01 (PROD)' });
 });
 
 module.exports = router;

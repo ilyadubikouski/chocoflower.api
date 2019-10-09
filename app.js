5dc118aca01811888a044f680d900c5a7eb96741
ilya <dubikowsky@gmail.com>
2019-10-08 20:18:33 -0400

    Created default folders structure

--- app.js
+++ app.js
@@ -5,18 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require('./routes/testAPI');
-var AboutRouter = require('./routes/about');
-var CategoriesRouter = require('./routes/categories');
-var AddRouter = require('./routes/add');
+var indexRouter = require('./src/routes/index');
+var CategoriesRouter = require('./src/routes/categories');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
@@ -26,11 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/testAPI', testAPIRouter);
-app.use('/about', AboutRouter);
-app.use('/add', AddRouter);
-app.use('/categories', CategoriesRouter);
+app.use('/v1/categories', CategoriesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
